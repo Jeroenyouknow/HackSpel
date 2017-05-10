eadbe8dc7f8a66d8bd82b2d73e3c3099e2e498f0
perrydrums <psperryjanssen@gmail.com>
2017-05-10 12:02:27 +0200

    index delete, mainframe->index

--- mainframe/index.php
+++ mainframe/index.php
@@ -1,1 +1,49 @@
-Hoi!
+<?php
+
+session_start();
+
+if(!isset($_SESSION['timesCorrect'])){
+    $_SESSION['timesCorrect'] = 0;
+}
+
+if(isset($_POST['answer'])){
+    if($_POST['answer'] == $_POST['correctAnswer']){
+        if(!($_SESSION['timesCorrect'] >= 4)){
+            $_SESSION['timesCorrect']++;
+        }
+    }
+}
+
+$timesCorrect = $_SESSION['timesCorrect'];
+
+include("functions/showCode.php");
+include("functions/puzzles.php");
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Pyramid Test</title>
+        <link rel="stylesheet" href="style.css" type="text/css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    </head>
+    <body>
+
+        <script>
+            window.addEventListener('load', function(e){
+                document.addEventListener('contextmenu', event => event.preventDefault());
+            })
+        </script>
+
+        <div id="code">
+
+        <?php
+
+        puzzle($timesCorrect);
+
+        ?>
+
+    </body>
+</html>
